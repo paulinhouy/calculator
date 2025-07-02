cf1e7b3807c15e1de6070838857295b60dffae7f
Paulo <paulofilhorocha22@gmail.com>
2025-07-02 10:50:39 -0300

    factoryfunctions

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function criaCalculadora(){
+    return {
+
+        display:document.querySelector( '.display'),
+
+        inicia(){
+            alert('oi iniciei')
+        },
+        cliqueBotoes(){
+            document.addEventListener('click',function(e){
+                const  el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay();
+                }
+            }.bind(this))
+        },
+        btnParaDisplay(){
+
+        }
+        
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
